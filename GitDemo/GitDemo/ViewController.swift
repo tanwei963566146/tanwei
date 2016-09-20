87744b63c1712eea767e5d1d422d565365c3320e
tw963566146 <963566146@qq.com>
2016-09-20 10:12:20 +0800

    初始化上传 框架

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by tanwei on 16/9/20.
+//  Copyright © 2016年 tanwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
